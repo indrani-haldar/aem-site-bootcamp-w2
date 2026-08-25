6e872ac9f3cc322524d97e659fd772e1543cae54
sreelekha-24323 <inf24323@adobe.com>
2026-08-25 17:41:49 +0530

    Added dialogs and models for search fheader and footer

---
+++ core/src/main/java/com/adobe/aem/bootcamp/core/learner004/models/SearchHeaderModel.java
@@ -0,0 +1,35 @@
+package com.adobe.aem.bootcamp.core.learner004.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class SearchHeaderModel {
+
+    @ValueMapValue
+    private String heading;
+
+    @ValueMapValue
+    private String ctaLabel;
+
+    @ValueMapValue
+    private String ctaLink;
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public String getCtaLabel() {
+        return ctaLabel;
+    }
+
+    public String getCtaLink() {
+        return ctaLink;
+    }
+}
